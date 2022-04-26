fe581f26a0c3ea875552e92b59ca110aa895152d
Crowdbotics <team@crowdbotics.com>
2022-04-26 23:20:05 +0000

    Update models

--- backend/car_racer_34952/wsgi.py
+++ backend/car_racer_34952/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_racer_34952.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_racer_34952.settings")
 
 application = get_wsgi_application()
